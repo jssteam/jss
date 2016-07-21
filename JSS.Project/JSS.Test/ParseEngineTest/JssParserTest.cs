c60d3324226ad281f2823a0dbff06f8c81fffdfe
Mike Riemer <mikejr76@live.com>
2016-07-20 22:33:08 -0500

    fix initial code submit

---
+++ JSS.Project/JSS.Test/ParseEngineTest/JssParserTest.cs
@@ -0,0 +1,36 @@
+﻿using JSS.Framework.ParseEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JSS.Test.ParseEngineTest
+{
+    public class JssParserTest
+    {
+
+        const string TEST_STRING = "this is my test string";
+        const string TEST_FILE_PATH = "testfile.txt";
+
+        JssParser _parser;
+
+        public JssParserTest()
+        {
+            _parser = new JssParser(TEST_FILE_PATH);
+
+        }//ctor
+
+        //Arrange, Act, Assert
+
+        [Fact]
+        public async Task ReadFileAsync_Path_String()
+        {
+            string retString = await _parser.ReadFileAsync();
+            Assert.Equal(TEST_STRING, retString);
+
+        }//ReadFileAsync_Path_String()
+
+    }//class JssParserTest
+
+}//namespace
---
+++ JSS.Project/src/JSS.Framework/ParseEngine/JssParseEngineException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSS.Framework.ParseEngine
+{
+
+    [Serializable]
+    public class JssParseEngineException : Exception
+    {
+        public JssParseEngineException() { }
+        public JssParseEngineException(string message) : base(message) { }
+        public JssParseEngineException(string message, Exception inner) : base(message, inner) { }
+        protected JssParseEngineException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info.ToString()) { }
+    }
+}
